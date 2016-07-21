a2dbd7999422567e3fd56e8600006291315fca57
Park9eon <quiet9.noise@gmail.com>
2016-07-21 19:42:13 +0900

    update rx module

--- app/src/main/java/sexy/park9eon/rx/MainActivity.java
+++ app/src/main/java/sexy/park9eon/rx/MainActivity.java
@@ -1,38 +1,89 @@
 package sexy.park9eon.rx;
 
+import android.bluetooth.BluetoothGattCharacteristic;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.widget.Button;
+import android.widget.EditText;
+
+import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText editText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        editText = (EditText) findViewById(R.id.editText);
+        scan();
+    }
+
+    public void scan() {
         findViewById(R.id.button).setOnClickListener(view -> {
-            RxBluetooth.create(getApplicationContext())
-                    .scan(new RxBluetooth.RxScanner() {
+            RxBluetooth.scan(getApplicationContext(), new RxBluetooth.BluetoothSubscriber<RxBluetooth.Result>() {
 
-                        @Override
-                        public void onCompleted() {
+                @Override
+                public void onCompleted() {
 
+                }
+
+                @Override
+                public void onError(Throwable e) {
+                    e.printStackTrace();
+                }
+
+                @Override
+                public void onNext(RxBluetooth.Result result) {
+                    if (result.getDevice().getName() == null) {
+                        Log.d("TEST", result.getDevice().getAddress() + " : null");
+                    } else {
+                        Log.d("TEST", result.getDevice().getAddress() + " : " + result.getDevice().getName());
+                        if (result.getDevice().getName().equals("iDL")) {
+                            getRxBluetooth().stopScan();
+                            getRxBluetooth().connect(result.getDevice(), new RxBluetooth.BluetoothSubscriber<BluetoothGattCharacteristic>() {
+                                @Override
+                                public void onStart() {
+                                    super.onStart();
+                                    Button button = (Button) findViewById(R.id.button);
+                                    button.setText(result.getDevice().getAddress());
+                                    button.setOnClickListener(view1 -> {
+                                        try {
+                                            this.getRxBluetooth().sendMessage(editText.getText().toString());
+                                        } catch (Exception e){
+                                            onError(e);
+                                        }
+                                    });
+                                    button.setOnLongClickListener(view1 -> {
+                                        this.getRxBluetooth().disconnect();
+                                        button.setText("re scan");
+                                        scan();
+                                        return true;
+                                    });
+                                }
+
+                                @Override
+                                public void onCompleted() {
+
+                                }
+
+                                @Override
+                                public void onError(Throwable e) {
+                                    e.printStackTrace();
+                                }
+
+                                @Override
+                                public void onNext(BluetoothGattCharacteristic bluetoothGattCharacteristic) {
+                                    Log.d("Request", Arrays.toString(bluetoothGattCharacteristic.getValue()));
+                                }
+                            });
                         }
-
-                        @Override
-                        public void onError(Throwable e) {
-                            e.printStackTrace();
-                        }
-
-                        @Override
-                        public void onNext(RxBluetooth.Result result) {
-                            if (result.getDevice().getName() == null) {
-                                Log.d("TEST", result.getDevice().getAddress() + " : null");
-                            } else {
-                                Log.d("TEST", result.getDevice().getAddress() + " : " + result.getDevice().getName());
-                            }
-                        }
-                    });
+                    }
+                }
+            });
         });
+
     }
 }
--- app/src/main/java/sexy/park9eon/rx/RxBluetooth.java
+++ app/src/main/java/sexy/park9eon/rx/RxBluetooth.java
@@ -9,18 +9,38 @@ import rx.Observable;
 import rx.Subscriber;
 import rx.functions.Action0;
 
-public final class RxBluetooth extends Observable<RxBluetooth.Result> {
+import java.util.UUID;
+
+public final class RxBluetooth<T> extends Observable<T> {
+
+    public static final UUID RX_SERVICE_UUID = UUID.fromString("6e400001-b5a3-f393-e0a9-e50e24dcca9e");
+    public static final UUID RX_CHAR_UUID = UUID.fromString("6e400002-b5a3-f393-e0a9-e50e24dcca9e");
+    public static final UUID TX_CHAR_UUID = UUID.fromString("6e400003-b5a3-f393-e0a9-e50e24dcca9e");
+    public static final UUID CCCD = UUID.fromString("00002902-0000-1000-8000-00805f9b34fb");
+
+    public static abstract class BluetoothSubscriber<T> extends Subscriber<T> {
+
+        private RxBluetooth<T> rxBluetooth;
+
+        private RxBluetooth<T> subscribe(RxBluetooth<T> rxBluetooth) {
+            this.rxBluetooth = rxBluetooth;
+            this.rxBluetooth.subscribe(this);
+            return this.rxBluetooth;
+        }
+
+        public RxBluetooth<T> getRxBluetooth() {
+            return this.rxBluetooth;
+        }
+    }
 
     public static class Result {
 
-        private final RxBluetooth rx;
         private final BluetoothDevice device;
         private final int rssi;
 
-        public Result(BluetoothDevice device, int rssi, RxBluetooth rx) {
+        public Result(BluetoothDevice device, int rssi) {
             this.device = device;
             this.rssi = rssi;
-            this.rx = rx;
         }
 
         public BluetoothDevice getDevice() {
@@ -33,29 +53,33 @@ public final class RxBluetooth extends Observable<RxBluetooth.Result> {
 
     }
 
-    private static final class BluetoothSubscriber implements Observable.OnSubscribe<Result>, Action0 {
+    private static final class RxScanner implements Observable.OnSubscribe<Result>, Action0 {
 
         private Object scanner;
-        private RxBluetooth rxBluetooth;
+        private RxBluetooth<Result> rxBluetooth;
 
-        public BluetoothSubscriber(Context context) {
-            this.rxBluetooth = new RxBluetooth(context, this);
+        public RxScanner(Context context) {
+            this.rxBluetooth = new RxBluetooth<>(context, this);
             this.rxBluetooth.doOnSubscribe(this);
         }
 
         @Override
         public void call(Subscriber<? super Result> subscriber) {
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-                this.scanner = new ScanCallback() {
-                    @Override
-                    public void onScanResult(int callbackType, ScanResult result) {
-                        subscriber.onNext(new Result(result.getDevice(), result.getRssi(), rxBluetooth));
-                    }
-                };
+            if (rxBluetooth.isEnable()) {
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                    this.scanner = new ScanCallback() {
+                        @Override
+                        public void onScanResult(int callbackType, ScanResult result) {
+                            subscriber.onNext(new Result(result.getDevice(), result.getRssi()));
+                        }
+                    };
+                } else {
+                    this.scanner = (BluetoothAdapter.LeScanCallback) (bluetoothDevice, i, bytes) -> subscriber.onNext(new Result(bluetoothDevice, i));
+                }
+                this.rxBluetooth.startScan(this.scanner);
             } else {
-                this.scanner = (BluetoothAdapter.LeScanCallback) (bluetoothDevice, i, bytes) -> subscriber.onNext(new Result(bluetoothDevice, i, rxBluetooth));
+                subscriber.onError(new Exception("Device not fount Exception"));
             }
-            this.rxBluetooth.startScan(this.scanner);
         }
 
         // 종료시 발동
@@ -66,36 +90,97 @@ public final class RxBluetooth extends Observable<RxBluetooth.Result> {
         }
     }
 
-    public static abstract class RxScanner extends Subscriber<Result> {
+    public static class RxConnector implements Observable.OnSubscribe<BluetoothGattCharacteristic>, Action0 {
 
-        private RxBluetooth rxBluetooth;
+        private RxBluetooth<android.bluetooth.BluetoothGattCharacteristic> rxBluetooth;
+        private BluetoothGattCallback callback;
+        private BluetoothDevice device;
 
-        public void subscribe(RxBluetooth rxBluetooth) {
-            this.rxBluetooth = rxBluetooth;
-            this.rxBluetooth.subscribe(this);
+        public RxConnector(Context context, BluetoothDevice device) {
+            this.rxBluetooth = new RxBluetooth<>(context, this);
+            this.device = device;
         }
 
-        public void stopScan() {
-            unsubscribe(); // BluetoothSubscriber 호출!
+        @Override
+        public void call(Subscriber<? super BluetoothGattCharacteristic> subscriber) {
+            callback = new BluetoothGattCallback() {
+
+                @Override
+                public void onConnectionStateChange(BluetoothGatt gatt, int status, int newState) {
+                    if (newState == BluetoothProfile.STATE_CONNECTED) {
+                        rxBluetooth.bluetoothGatt.discoverServices();
+                    } else if (newState == BluetoothProfile.STATE_DISCONNECTED) {
+                        if (rxBluetooth.bluetoothGatt != null) {
+                            rxBluetooth.bluetoothGatt.close();
+                        }
+                    }
+                }
+
+                @Override
+                public void onCharacteristicRead(BluetoothGatt gatt, BluetoothGattCharacteristic characteristic, int status) {
+                    if (status == BluetoothGatt.GATT_SUCCESS) {
+                        subscriber.onNext(characteristic);
+                    }
+                }
+
+                @Override
+                public void onCharacteristicChanged(BluetoothGatt gatt, BluetoothGattCharacteristic characteristic) {
+                    subscriber.onNext(characteristic);
+                }
+
+                @Override
+                public void onCharacteristicWrite(BluetoothGatt gatt, BluetoothGattCharacteristic characteristic, int status) {
+                    subscriber.onNext(characteristic);
+                }
+
+                @Override
+                public void onServicesDiscovered(BluetoothGatt gatt, int status) {
+                    if (status == BluetoothGatt.GATT_SUCCESS) {
+                        rxBluetooth.enableNotification();
+                    }
+                }
+            };
+            this.rxBluetooth.connect(this.device, callback);
+        }
+
+        @Override
+        public void call() {
+            // TODO
+            this.rxBluetooth.disconnect();
         }
     }
 
     private final BluetoothManager manager;
     private final Context context;
     private Object scanner = null;
+    private BluetoothGatt bluetoothGatt = null;
 
-    private RxBluetooth(Context context, BluetoothSubscriber subscriber) {
+    private RxBluetooth(Context context, OnSubscribe<T> subscriber) {
         super(subscriber);
         this.context = context;
         this.manager = (BluetoothManager) context.getSystemService(Context.BLUETOOTH_SERVICE);
     }
 
-    public static RxBluetooth create(Context context) {
-        return new BluetoothSubscriber(context).rxBluetooth;
+    public static RxBluetooth<Result> scan(Context context, BluetoothSubscriber<Result> subscriber) {
+        return subscriber.subscribe(new RxScanner(context).rxBluetooth);
     }
 
-    public void scan(RxScanner scanner) {
-        scanner.subscribe(this);
+    protected boolean isEnable() {
+        return this.manager != null && this.manager.getAdapter().isEnabled();
+    }
+
+    protected RxBluetooth<BluetoothGattCharacteristic> connect(BluetoothDevice device, BluetoothSubscriber<BluetoothGattCharacteristic> subscriber) {
+        this.stopScan();
+        return subscriber.subscribe(new RxConnector(context, device).rxBluetooth);
+    }
+
+    protected void connect(BluetoothDevice device, BluetoothGattCallback callback) {
+        this.bluetoothGatt = device.connectGatt(this.context, false, callback);
+        this.bluetoothGatt.connect();
+    }
+
+    protected void disconnect() {
+        this.bluetoothGatt.disconnect();
     }
 
     private void startScan(Object o) {
@@ -107,12 +192,32 @@ public final class RxBluetooth extends Observable<RxBluetooth.Result> {
         this.scanner = o;
     }
 
-    private void stopScan() {
+    public void stopScan() {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
             this.manager.getAdapter().getBluetoothLeScanner().stopScan((ScanCallback) this.scanner);
         } else {
             this.manager.getAdapter().stopLeScan((BluetoothAdapter.LeScanCallback) this.scanner);
         }
+    }
+
+    public void sendMessage(String message) {
+        this.sendMessage(message.getBytes());
+    }
+
+    public void sendMessage(byte[]message) {
+        BluetoothGattService gattService = this.bluetoothGatt.getService(RX_SERVICE_UUID);
+        BluetoothGattCharacteristic characteristic = gattService.getCharacteristic(RX_CHAR_UUID);
+        characteristic.setValue(message);
+        this.bluetoothGatt.writeCharacteristic(characteristic);
+    }
+
+    public void enableNotification() {
+        BluetoothGattService service = this.bluetoothGatt.getService(RX_SERVICE_UUID);
+        BluetoothGattCharacteristic txCharacteristic = service.getCharacteristic(TX_CHAR_UUID);
+        this.bluetoothGatt.setCharacteristicNotification(txCharacteristic, true);
+        BluetoothGattDescriptor descriptor = txCharacteristic.getDescriptor(CCCD);
+        descriptor.setValue(BluetoothGattDescriptor.ENABLE_NOTIFICATION_VALUE);
+        this.bluetoothGatt.writeDescriptor(descriptor);
     }
 
 }
